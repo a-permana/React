fc9351634b4a48ca28735f0931129e485daba3cc
alek permana <1alekpermana@gmail.com>
2020-02-20 15:15:53 +0700

    clean

--- src/Challenge/avatar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function avatar() {
-    return (
-        <div>
-            <img src ="avatar.png" class="mx-auto d-block" alt="avatar" width="300"></img>
-        </div>
-    )
-}
