25795fade3ea3b52c733742a595c873e64c4237c
alek permana <1alekpermana@gmail.com>
2020-02-20 15:45:07 +0700

    clear

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import './App.css';
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-// import { Container, Row, Col } from "reacstrap";
 import './index';
-import PostBook from './hook/PostBook';
+import Get from './hook/get';
+
+// import PostBook from './hook/PostBook';
 
 const App = props => {
   return (
     <div>
+        <Get/>
         {/* <FetchHook/> */}
         {/* <PostBook/> */}
-        {/* <Get/> */}
         {/* <FetchUsingHook/> */}
     </div>
-
   );
 };
 export default App;
